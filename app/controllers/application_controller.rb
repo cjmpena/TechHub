3d2b3a4ffe0e29773c017701c0aa43aa54e5b5ee
John Paul Villanueva <jpvillanueva2000@gmail.com>
2023-11-16 22:25:54 -0600

    add, edit, or delete product listings on admin dashboard and implement front page navigation for customers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
-end
+    protect_from_forgery with: :exception
+  
+    def after_sign_in_path_for(resource)
+      if resource.is_a?(AdminUser)
+        admin_dashboard_path
+      else
+        products_path
+      end
+    end
+  end
+  
--- app/controllers/customers/sessions/sessions_controller.rb
+++ app/controllers/customers/sessions/sessions_controller.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
-class Customers::SessionsController < Devise::SessionsController
+class Customers::Sessions::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-  
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || session[:return_to] || root_path
+  end
+
   # GET /resource/sign_in
   # def new
   #   super
