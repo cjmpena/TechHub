1563e72a8ebca13717e8d3f28ce0a7da993ecc39
Carla <manansal.carla@gmail.com>
2023-12-14 10:22:49 -0600

    Removed unessecary comments

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,7 +4,6 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  # Allows for data to display and add on the customer tab  
   def self.ransackable_attributes(auth_object = nil)
     %w[id first_name last_name email phone address created_at updated_at]
   end
